c4552529d9b47888895ec03c1bc2c2a139ca42e3
Ka Ho Wu <kahowu@KadeMacBook-Pro.lan>
2023-04-28 03:03:07 -0400

    Added both chinese and english language support

--- collab_gpt.py
+++ collab_gpt.py
@@ -70,9 +70,9 @@ def call_chatgpt_api(user_input, messages, role):
         return call_chatgpt_api(user_input, messages, role)
 
 # Read the basic prompt from the file
-def read_basic_prompt():
+def read_basic_prompt(language):
     combined_lines = ""
-    with open("basic_prompt.txt", "r") as file:
+    with open("basic_prompt_" + language + ".txt", "r") as file:
         # Iterate over each line in the file
         for line in file:
             # Add the line to the combined_lines string
@@ -95,7 +95,8 @@ def main():
     # while True:
         # Ask if the user want to load template or not
     output_folder_path = "outputs/"
-    load_template = input("Do you want to load personas template? (y/n): ").upper()
+    language = input("Enter the language of for the following discussion English/Chinese): ").lower()
+    load_template = input("Do you want to load previous personas template? (y/n): ").upper()
     print ("\n")
     if load_template == "N":
         while True:
@@ -105,7 +106,7 @@ def main():
             except ValueError:
                 print("Invalid input. Please enter a valid integer for the number of personas.")
         chatbots = []
-        basic_prompt = read_basic_prompt()
+        basic_prompt = read_basic_prompt(language)
         for i in range(num_chatbots):
             name = ""
             while not name.strip():
@@ -138,13 +139,14 @@ def main():
     # Ask the user to input a file name
     file_name = input("Enter a file name to save the discussions: ")
 
-    goal = input("The goal of this conversation is: ")
+    goal = input("The goal of this discussion is: ")
     output_file = file_name + ".txt"
     # output file is stored in the folder output, open and write the goal of the conversation
     output_path = output_folder_path + output_file
     # Create a new file named output_path
     with open(output_path, "w") as file:
-        file.write(f"The goal of this conversation is: {goal} \n\n")
+        file.write(f"The goal of this discussion is: {goal} \n\n")
+
 
     conversation_history = [{"role": "system", "content": f"All of you are trying to accomplish this {goal}"}]
     # Enter the number of rounds of conversation, by default it is infinite
@@ -157,7 +159,11 @@ def main():
             print ("Initial discussion...\n") 
             print (f"Round {round} of discussion...\n")               
             for chatbot in chatbots:
-                full_prompt = f"Your name is {chatbot['name']}. {chatbot['prompt']} You are responsible for {chatbot['task']} Accomplish this {goal}."
+                # English version of the prompt
+                if language == "english":
+                    full_prompt = f"Your name is {chatbot['name']}. {chatbot['prompt']} You are responsible for {chatbot['task']} Accomplish this {goal}."
+                elif language == "chinese":
+                    full_prompt = f"你的名字是 {chatbot['name']}. {chatbot['prompt']}. 你要完成此任务 {chatbot['task']} 并达成此目标 {goal}."
                 response = call_chatgpt_api(full_prompt, conversation_history, chatbot['role'])
                 conversation_history.append({"role": chatbot['role'], "content": response})
                 line = f"{chatbot['name']}: \n" + response + "\n\n"
@@ -168,14 +174,32 @@ def main():
         elif (round > 1 and round <= num_rounds) or num_rounds == -1:
             print ("Continuing the discussion...\n")
             print (f"Round {round} of discussion...\n")
-            for chatbot in chatbots:
-                full_prompt = f"Your name is {chatbot['name']}. {chatbot['prompt']}. You are responsible for {chatbot['task']} Generate more contents on previous contexts, go into deeper discussions about the previous contexts. Criticize the other users about their opinions if you don't agree, don't be easily agreeable. Stop saying thank you. Don't repeat yourself. Ask others question based on previous contexts. Use number and statistics to support your claims."
-                response = call_chatgpt_api(full_prompt, conversation_history, chatbot['role'])
-                conversation_history.append({"role": chatbot['role'], "content": response})
-                line = f"{chatbot['name']}: \n" + response + "\n\n"
-                conversation_to_text_file (line, output_path)
-                print(f"{chatbot['name']}: \n{response}\n")
-                print ("\n")
+            # if round is equal to num_rounds, then the chatbots will end the discussion and summerize their views. 
+            if round == num_rounds:
+                print ("Ending the discussion...\n")
+                for chatbot in chatbots:
+                    if language == "english":
+                        full_prompt = f"Your name is {chatbot['name']}. {chatbot['prompt']}. Base on previous contexts, summerize your views and you opinions on other users' views."
+                    elif language == "chinese":
+                        full_prompt = f"你的名字是 {chatbot['name']}. {chatbot['prompt']}. 基于之前的上下文，总结你的观点和总结你对其他用户观点的看法."
+                    response = call_chatgpt_api(full_prompt, conversation_history, chatbot['role'])
+                    conversation_history.append({"role": chatbot['role'], "content": response})
+                    line = f"{chatbot['name']}: \n" + response + "\n\n"
+                    conversation_to_text_file (line, output_path)
+                    print(f"{chatbot['name']}: \n{response}\n")
+                    print ("\n")
+            elif round < num_rounds:
+                for chatbot in chatbots:
+                    if language == "english":
+                        full_prompt = f"Your name is {chatbot['name']}. {chatbot['prompt']}. You are responsible for {chatbot['task']} Generate more contents on previous contexts, go into deeper discussions about the previous contexts. Criticize the other users about their opinions if you don't agree, don't be easily agreeable. Stop saying thank you. Don't repeat yourself. Ask others question based on previous contexts. Use number and statistics to support your claims."
+                    elif language == "chinese":
+                        full_prompt = f"你的名字是 {chatbot['name']}. {chatbot['prompt']}. 你要完成此任务 {chatbot['task']} 基于上下文，生成更多内容并进行深入讨论。批判对方观点，刁难对方，挑战对方。不要轻易同意任何人观点。不要说谢谢。不要重复自己。基于上下文向其它用户提出更多尖锐的问题。使用数字和统计数据支持您的观点。"
+                    response = call_chatgpt_api(full_prompt, conversation_history, chatbot['role'])
+                    conversation_history.append({"role": chatbot['role'], "content": response})
+                    line = f"{chatbot['name']}: \n" + response + "\n\n"
+                    conversation_to_text_file (line, output_path)
+                    print(f"{chatbot['name']}: \n{response}\n")
+                    print ("\n")
         # Reach the max number of rounds
         elif round > num_rounds and num_rounds != -1:
             # Ask the user for the next action
@@ -225,7 +249,7 @@ def main():
 
 
 if __name__ == "__main__":
-    print("\nWelcome to the Collab Room! Here you could create multiple intelligent chat personas to work together to accomplish a certain goal, or just ask them to have a fun discussion about a given topic! \n")
+    print("\nWelcome to the CollabGPT! Here you could create multiple intelligent chat personas to work together to accomplish a certain goal, or just ask them to have a fun discussion about a given topic! \n")
     if not OPENAI_API_KEY:
         print("Your OPENAI_API_KEY is not set as an environment variable.")
         OPENAI_API_KEY = getpass("Please enter your OpenAI API key: ")
